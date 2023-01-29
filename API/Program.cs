a1ca8167d11be6a776808c10f1212bbe9ae006b4
Davi Arruda <daviarruda_@hotmail.com>
2023-01-29 11:52:36 -0300

    Adding Migrations AddDescriptionOnProduct and ConfigureTableProduct

--- API/Program.cs
+++ API/Program.cs
@@ -64,12 +64,22 @@ public class Product
     public int Id { get; set; }
     public string Code { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
 }
 
 public class ApplicationDbContext : DbContext 
 {
     public DbSet<Product> Products {get; set;}
 
+protected override void OnModelCreating(ModelBuilder builder)
+{
+    builder.Entity<Product>()
+        .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
+    builder.Entity<Product>()
+        .Property(P => P.Name).HasMaxLength(120).IsRequired();
+    builder.Entity<Product>()
+        .Property(p => p.Code).HasMaxLength(20).IsRequired();
+}
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=@Sql2023;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES"); 
 }
