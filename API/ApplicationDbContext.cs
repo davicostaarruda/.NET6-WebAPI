1e06466f6edd8861dfbbd152194b01b3ef0b5556
Davi Arruda <daviarruda_@hotmail.com>
2023-01-31 22:50:49 -0300

    Adding Migrations RenameCategoryToCategories and Adding new product

--- API/ApplicationDbContext.cs
+++ API/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 public class ApplicationDbContext : DbContext 
 {
     public DbSet<Product> Products {get; set;}
+    public DbSet<Category> Categories {get; set;}
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options){
 
@@ -16,5 +17,7 @@ public class ApplicationDbContext : DbContext
             .Property(P => P.Name).HasMaxLength(120).IsRequired();
         builder.Entity<Product>()
             .Property(p => p.Code).HasMaxLength(20).IsRequired();
+        builder.Entity<Category>()
+            .ToTable("Categories");
     }
 }
